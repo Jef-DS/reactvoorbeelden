1f3ffcd4021a8ed46e23c2a92b31e3f47399f5ed
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2019-05-24 14:09:18 +0200

    voorbeeld lijst van componenten

--- jsxcomponenten/src/App.js
+++ jsxcomponenten/src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import './App.css';
-//voegt een link toe naar de bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const items = [
+    {href: "http://www.cevora.be", tekst: "Naar Cevora"},
+    {href: "http://www.betavzw.org", tekst: "Naar Beta"}
+  ]
   return (
-    <MijnLink href="http://www.cevora.be" target="_blank" tekst="Naar Cevora"/>
+    //Een render()-functie kan maar één root-element teruggeven
+    //<React.Fragment> genereert geen <html>-code
+    <React.Fragment>
+      <MijnLink href="http://www.cevora.be" target="_blank" tekst="Naar Cevora"/>
+      <MijnList items={items}/>
+    </React.Fragment>
   );
 }
 
+function MijnList(props){
+  return(
+    <ul>
+      {
+          // Een array van objecten omzetten naar React-elementen
+          // kan gemakkelijk met de Array.prototype.map()-functie
+          props.items.map((item, index) => 
+          // in een lijst met childelementen moet elk element een unieke key
+          // hebben. In feite is de index van het element geen goede keuze,
+          // maar voor dit voorbeeld kan het wel.
+          <li key={index}><MijnLink href={item.href} tekst={item.tekst}/></li>
+        )
+      }
+    </ul>
+  )
+}
 function MijnLink(props) {
-  // haal de tekst-property uit het object
-  // en bewaar de andere properties in a_props
-  // dit wordt beschouwd als een antipattern omdat we niet weten
-  // welke properties aanwezig zijn in 'props'
-
-  /* const {tekst, ...a_props} = {...props};
-   * return ( 
-   * <a {...a_props}>{tekst}</a>
-   *)
-   */
-
-  // Hier weten we exact wat we aan het doen zijn. 
-  // (maar de toetsen van je computer en je vingertoppen verslijten meer)
   const {tekst, href, target} = {...props};
   return (
     <a href={href} target={target}>{tekst}</a>
