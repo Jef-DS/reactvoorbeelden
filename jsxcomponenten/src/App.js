3c87fb8dc8ac0c89e86339e7662e45f57cbeb39a
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2019-05-24 13:33:12 +0200

    voorbeeld properties

--- jsxcomponenten/src/App.js
+++ jsxcomponenten/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//voegt een link toe naar de bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MijnLink href="http://www.cevora.be" target="_blank" tekst="Naar Cevora"/>
   );
 }
 
+function MijnLink(props) {
+  // haal de tekst-property uit het object
+  // en bewaar de andere properties in a_props
+  // dit wordt beschouwd als een antipattern omdat we niet weten
+  // welke properties aanwezig zijn in 'props'
+  
+  /* const {tekst, ...a_props} = {...props};
+   * return ( 
+   * <a {...a_props}>{tekst}</a>
+   *)
+   */
+  const {tekst, href, target} = {...props};
+  return (
+    <a href={href} target={target}>{tekst}</a>
+  )
+}
 export default App;
--- jsxcomponenten/src/index.js
+++ jsxcomponenten/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// voeg de App component/functie toe als child van #root in index.html
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
